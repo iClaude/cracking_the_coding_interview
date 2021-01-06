423bc415eb55100b32071d74b68cd99c1266d612
iClaude <agostini.claudio1@gmail.com>
2021-01-06 15:15:19 +0100

    add solution to exercise 1.5b

--- src/chapter01/01_05b.kt
+++ src/chapter01/01_05b.kt
@@ -1,27 +1,43 @@
 package chapter01
 
+import kotlin.math.abs
+
 /*
-    String Compression: Implement a method to perform basic string compression using the counts
-    of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the
-    "compressed" string would not become smaller than the original string, your method should return
-    the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+    1.5 One Away: There are three types of edits that can be performed on strings: insert a character,
+        remove a character, or replace a character. Given two strings, write a function to check if
+        they are one edit (or zero edits) away.
+        EXAMPLE
+        pale, ple -> true
+        pales, pale -> true
+        pale, bale -> true
+        pale, bake -> false
  */
 
-private fun compressString(str: String): String {
-    val sb = StringBuilder()
+private fun isOneEditAway(mStr1: String, mStr2: String): Boolean {
+    if (abs(mStr1.length - mStr2.length) > 1) return false
+
+    val str1 = if (mStr1.length > mStr2.length) mStr1 else mStr2
+    val str2 = if (mStr1.length > mStr2.length) mStr2 else mStr1
+
+    var edits = 0
     var i = 0
-    var len = 0
-    while (i in str.indices) {
-        sb.append(str[i])
-        var j = i + 1
-        while (j in str.indices && str[j] == str[i]) {
+    var j = 0
+    while (i in str1.indices && j in str2.indices) {
+        if (str1[i] == str2[j]) {
+            i++
             j++
+            continue
         }
-        sb.append("${j - i}")
-        len += 1 + "${j - i}".length
-        if (len >= str.length) return str
-        i = j
+
+        if (i + 1 in str1.indices && j + 1 in str2.indices && str1[i + 1] == str2[j + 1]) j++
+        edits++
+        i++
+
+        if (edits > 1) return false
     }
 
-    return sb.toString()
+    edits += str1.length - i
+
+    return edits < 2
 }
+
