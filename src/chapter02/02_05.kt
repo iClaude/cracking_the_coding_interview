912806904e7a79898be6cdb51c638805d8ee614b
iClaude <agostini.claudio1@gmail.com>
2021-02-11 15:04:59 +0100

    add solution of exercise 2.5 (still to finish)

--- src/chapter02/02_05.kt
+++ src/chapter02/02_05.kt
@@ -1,47 +1,74 @@
 package chapter02
 
-/*
-  2.5 Given a circular linked list, implement an algorithm which returns node at the beginning of the loop.
-  DEFINITION
-  Circular linked list: A (corrupt) linked list in which a node’s next pointer points to an earlier node, so as to make a loop in the linked list.
-  EXAMPLE
-  input: A -> B -> C -> D -> E -> C [the same C as earlier]
-  output: C
-
-  ALGORITHM (function findLoopBegin())
-  Use 2 pointers: p1 moves one node at a time, p2 moves twice as fast (2 nodes at a time). Let them scan the list. The 2 pointers will meet at
-  some point at a specific position: the distance between this position and the beginning of the pool is exactly the same as the distance
-  between the head of the list and the beginning of the loop.
-  Now use 2 pointers, p1 starting from the found position, and p3 starting from the head, and move them at the same speed. They will meet
-  exactly at the beginning of the loop.
-  Performance: O(n)
-*/
-
 fun main() {
-    val list = SinglyLinkedList<Char>()
-    list.pushBack('a')
-    list.pushBack('b')
-    val node1 = list.pushBackWithNode('c')
-    list.pushBack('d')
-    val node2 = list.pushBackWithNode('e')
-    node2.next = node1
-
-    println(findLoopBegin(list).key)
-}
-
-fun <T> findLoopBegin(list: SinglyLinkedList<T>): Node<T> {
-    var p1 = list.head!!.next!!
-    var p2 = list.head!!.next!!.next!!
-    while (p1 != p2) {
-        p1 = p1.next!!
-        p2 = p2.next!!.next!!
+    val head1 = LinkedNode(2).apply {
+        next = LinkedNode(8).apply {
+            next = LinkedNode(7)
+        }
+    }
+    val head2 = LinkedNode(1).apply {
+        next = LinkedNode(3)
     }
 
-    var p3 = list.head!!
-    while (p1 != p3) {
-        p1 = p1.next!!
-        p3 = p3.next!!
+    printList(sumLists(head1, head2))
+    println()
+    printList(sumListsForwardOrder(head1, head2))
+
+}
+
+private fun sumLists(head1: LinkedNode, head2: LinkedNode): LinkedNode {
+    val head = LinkedNode(sum(head1.value, head2.value))
+    var rem = remainder(head1.value, head2.value)
+
+    var p = head
+    var p1 = head1.next
+    var p2 = head2.next
+
+    while (p1 != null || p2 != null) {
+        val node = LinkedNode(sum(p1?.value ?: 0, p2?.value ?: 0, rem))
+        rem = remainder(p1?.value ?: 0, p2?.value ?: 0, rem)
+        p.next = node
+        p = node
+
+        p1 = p1?.next
+        p2 = p2?.next
     }
 
-    return p1
+    if (rem == 1) {
+        p.next = LinkedNode(1)
+    }
+
+    return head
 }
+
+private fun sum(vararg nums: Int) =
+        nums.sum() % 10
+
+private fun remainder(vararg nums: Int) =
+        if (nums.sum() >= 10) 1 else 0
+
+private fun printList(head: LinkedNode?) {
+    var p: LinkedNode? = head
+    while (p != null) {
+        print("${p.value} - ")
+        p = p.next
+    }
+}
+
+private fun sumListsForwardOrder(node1: LinkedNode?, node2: LinkedNode?): LinkedNode? {
+    if (node1 == null && node2 == null) {
+        return null
+    }
+
+    val node = sumListsForwardOrder(node1?.next, node2?.next)
+
+    val mNode = LinkedNode(sum(node1?.value ?: 0, node2?.value ?: 0, remF)).apply {
+        next = node
+    }
+    remF = remainder(node1?.value ?: 0, node2?.value ?: 0, remF)
+
+    return mNode
+
+}
+
+var remF = 0
