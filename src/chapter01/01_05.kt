cefc95d5275b97f3227b567dd9cdd0aaace5d9de
iClaude <agostini.claudio1@gmail.com>
2021-02-21 18:23:00 +0100

    improve solution of exercise 1.5

--- src/chapter01/01_05.kt
+++ src/chapter01/01_05.kt
@@ -15,16 +15,16 @@ import kotlin.math.abs
 fun main() {
     val str1 = "apple"
     val str2 = "aple"
-    println(isMaxOneEditAway(str1, str2))
+    println(areOneEditAway(str1, str2))
     println(isMaxOneEditAwayCombined(str1, str2))
 }
 
-private fun isMaxOneEditAway(str1: String, str2: String): Boolean {
-    if (abs(str1.length - str2.length) > 1) return false
-
-    if (str1.length == str2.length) return checkReplace(str1, str2)
-
-    return checkInsert(str1, str2)
+private fun areOneEditAway(str1: String, str2: String): Boolean {
+    return if (str1.length == str2.length) {
+        checkReplace(str1, str2)
+    } else {
+        checkInsertOrRemove(str1, str2)
+    }
 }
 
 private fun checkReplace(str1: String, str2: String): Boolean {
@@ -38,23 +38,23 @@ private fun checkReplace(str1: String, str2: String): Boolean {
     return true
 }
 
-private fun checkInsert(pStr1: String, pStr2: String): Boolean {
-    val str1 = if (pStr1.length > pStr2.length) pStr1 else pStr2
-    val str2 = if (pStr2.length < pStr1.length) pStr2 else pStr1
+private fun checkInsertOrRemove(str1: String, str2: String): Boolean {
+    if (abs(str1.length - str2.length) > 1) return false
 
-    var i = 0
-    var j = 0
-    var edits = 0
-    while (i in str1.indices && j in str2.indices) {
-        if (str1[i] != str2[j]) {
-            edits++
-            if (edits > 1) return false
+    val mStr1 = if (str1.length > str2.length) str1 else str2
+    val mStr2 = if (str1.length > str2.length) str2 else str1
+
+    var p1 = 0
+    var p2 = 0
+    while (p1 in mStr1.indices && p2 in mStr2.indices) {
+        if (mStr1[p1] == mStr2[p2]) {
+            p1++
+            p2++
         } else {
-            j++
+            p1++
+            if (p1 - p2 > 1) return false
         }
-        i++
     }
-
     return true
 }
 
--- src/leetcode/Recap.kt
+++ src/leetcode/Recap.kt
@@ -1,31 +1,48 @@
 package leetcode
 
+import kotlin.math.abs
+
 fun main() {
-    val str = "Mr John Smith                             ".toCharArray()
-    val trueLen = 13
-    urlify(str, trueLen)
-    println(str.joinToString(""))
+    val str1 = "pales"
+    val str2 = "paes"
+    println(areOneEditAway(str1, str2))
 }
 
-private fun urlify(str: CharArray, trueLen: Int) {
-    var index = getFinalIndex(str, trueLen)
+private fun areOneEditAway(str1: String, str2: String): Boolean {
+    return if (str1.length == str2.length) {
+        checkReplace(str1, str2)
+    } else {
+        checkInsertOrRemove(str1, str2)
+    }
+}
 
-    for (i in trueLen - 1 downTo 0) {
-        val char = str[i]
-        if (char == ' ') {
-            str[index--] = '0'
-            str[index--] = '2'
-            str[index--] = '%'
-        } else {
-            str[index--] = char
+private fun checkReplace(str1: String, str2: String): Boolean {
+    var edits = 0
+    for (i in str1.indices) {
+        if (str1[i] != str2[i]) {
+            edits++
+            if (edits > 1) return false
         }
     }
+    return true
 }
 
-private fun getFinalIndex(str: CharArray, trueLen: Int): Int {
-    var spaces = 0
-    for (i in 0 until trueLen) {
-        if (str[i] == ' ') spaces++
+private fun checkInsertOrRemove(str1: String, str2: String): Boolean {
+    if (abs(str1.length - str2.length) > 1) return false
+
+    val mStr1 = if (str1.length > str2.length) str1 else str2
+    val mStr2 = if (str1.length > str2.length) str2 else str1
+
+    var p1 = 0
+    var p2 = 0
+    while (p1 in mStr1.indices && p2 in mStr2.indices) {
+        if (mStr1[p1] == mStr2[p2]) {
+            p1++
+            p2++
+        } else {
+            p1++
+            if (p1 - p2 > 1) return false
+        }
     }
-    return trueLen - 1 + spaces * 2
+    return true
 }
