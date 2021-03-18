c6d641f2e90f5b46b33cdc2fce76c3668740e3fc
iClaude <agostini.claudio1@gmail.com>
2021-03-18 17:56:01 +0100

    recap of exercise 3.2 (work in progress)

--- src/leetcode/Exercise.kt
+++ src/leetcode/Exercise.kt
@@ -1,35 +1,80 @@
 package leetcode
 
+import kotlin.math.abs
+
 fun main() {
+    val str1 = "pale"
+    val str2 = "bake"
+    println(areOneEditAway(str1, str2))
+    println(areOneEditAwaySinglePass(str1, str2))
+}
+
+private fun areOneEditAway(str1: String, str2: String): Boolean {
+    if (abs(str1.length - str2.length) > 1) return false
+
+    return if (str1.length == str2.length) {
+        checkReplace(str1, str2)
+    } else {
+        checkInsertOrDelete(str1, str2)
+    }
 
 }
 
-class Solution {
-    fun kWeakestRows(mat: Array<IntArray>, k: Int): IntArray {
-        val rows = Array(mat.size) {
-            Row(it, mat[0].size)
+private fun checkReplace(str1: String, str2: String): Boolean {
+    var edits = 0
+    for (i in str1.indices) {
+        if (str1[i] != str2[i]) {
+            edits++
+            if (edits > 1) return false
         }
-
-        mat.forEachIndexed { index, arr ->
-            for (i in arr.indices) {
-                if (arr[i] == 0) break
-                rows[index].weakness--
-            }
-        }
-
-        val result = Array(k) { -1 }
-        rows.sortDescending()
-        for (i in 0 until k) {
-            result[i] = rows[i].index
-        }
-
-        return result.toIntArray()
     }
+    return true
 }
 
-data class Row(val index: Int, var weakness: Int) : Comparable<Row> {
-    override fun compareTo(other: Row): Int {
-        if (this.weakness != other.weakness) return this.weakness - other.weakness
-        return other.index - this.index
+private fun checkInsertOrDelete(mStr1: String, mStr2: String): Boolean {
+    val str1 = if (mStr1.length > mStr2.length) mStr1 else mStr2
+    val str2 = if (mStr1.length > mStr2.length) mStr2 else mStr1
+
+    var edits = 0
+    var i = 0
+    var j = 0
+    while (i in str1.indices && j in str2.indices) {
+        if (str1[i] == str2[j]) {
+            j++
+        } else {
+            edits++
+            if (edits > 1) return false
+        }
+        i++
     }
+
+    return true
+}
+
+private fun areOneEditAwaySinglePass(str1: String, str2: String): Boolean {
+    if (abs(str1.length - str2.length) > 1) return false
+
+    return checkSinglePass(str1, str2)
+
+}
+
+private fun checkSinglePass(mStr1: String, mStr2: String): Boolean {
+    val str1 = if (mStr1.length > mStr2.length) mStr1 else mStr2
+    val str2 = if (mStr1.length > mStr2.length) mStr2 else mStr1
+
+    var edits = 0
+    var i = 0
+    var j = 0
+    while (i in str1.indices && j in str2.indices) {
+        if (str1[i] == str2[j]) {
+            j++
+        } else {
+            edits++
+            if (edits > 1) return false
+            if (str1.length == str2.length) j++
+        }
+        i++
+    }
+
+    return true
 }
