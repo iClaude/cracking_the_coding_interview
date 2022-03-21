e30590d369bbad7b75d1ceef257463884439a67b
iClaude <agostini.claudio1@gmail.com>
2022-03-21 10:17:31 +0100

    exercise 1.6

--- src/chapter01/01_06.kt
+++ src/chapter01/01_06.kt
@@ -5,21 +5,78 @@ package chapter01
     "compressed" string would not become smaller than the original string, your method should return
     the original string. You can assume the string has only uppercase and lowercase letters (a - z).  */
 
+import kotlin.math.abs
+import kotlin.math.log10
+
+fun main() {
+    val str = "aaabbcccccdd"
+    println("compressed string = ${compressString(str)}")
+    println("compressed string = ${compressString2(str)}")
+
+}
+
 private fun compressString(str: String): String {
-    val sb = StringBuilder()
-    var i = 0
-    var len = 0
+    val sb = StringBuilder(str[0].toString())
+
+    var start = 0
+    var i = 1
     while (i in str.indices) {
-        sb.append(str[i])
-        var j = i + 1
-        while (j in str.indices && str[j] == str[i]) {
-            j++
+        if (str[i] != str[start]) {
+            sb.append(i - start)
+            sb.append(str[i])
+            start = i
         }
-        sb.append("${j - i}")
-        len += 1 + "${j - i}".length
-        if (len >= str.length) return str
-        i = j
+        i++
     }
+    sb.append(i - start)
+
+    return if (sb.length < str.length) {
+        sb.toString()
+    } else {
+        str
+    }
+}
+
+private fun compressString2(str: String): String {
+    val compressedLength = computeCompressedLength(str)
+    if (compressedLength >= str.length) return str
+
+    val sb = StringBuilder(compressedLength).also {
+        it.append(str[0])
+    }
+    var start = 0
+    var i = 1
+    while (i in str.indices) {
+        if (str[i] != str[start]) {
+            sb.append(i - start)
+            sb.append(str[i])
+            start = i
+        }
+        i++
+    }
+    sb.append(i - start)
 
     return sb.toString()
+}
+
+private fun computeCompressedLength(str: String): Int {
+    var len = 0
+
+    var start = 0
+    var i = 1
+    while (i in str.indices) {
+        if (str[i] != str[start]) {
+            len += 1 + (i - start).length()
+            start = i
+        }
+        i++
+    }
+    len += 1 + (i - start).length()
+
+    return len
+}
+
+fun Int.length() = when (this) {
+    0 -> 1
+    else -> log10(abs(toDouble())).toInt() + 1
 }
--- src/recap/Recap.kt
+++ src/recap/Recap.kt
@@ -1,69 +1,77 @@
 package recap
 
 import kotlin.math.abs
+import kotlin.math.log10
 
 fun main() {
-    val str1 = "pale"
-    val str2 = "bake"
-    println("Are the 2 strings one edit away? ${oneEditAway(str1, str2)}")
-    println("Are the 2 strings one edit away (one pass)? ${oneEditAwayOnePass(str1, str2)}")
+    val str = "aaabbcccccdd"
+    println("compressed string = ${compressString(str)}")
+    println("compressed string = ${compressString2(str)}")
 
 }
 
-private fun oneEditAway(str1: String, str2: String): Boolean {
-    if (abs(str1.length - str2.length) > 1) return false
+private fun compressString(str: String): String {
+    val sb = StringBuilder(str[0].toString())
 
-    return if (str1.length == str2.length) {
-        checkWithEqualLength(str1, str2)
-    } else {
-        checkWithDifferentLength(str1, str2)
-    }
-}
-
-private fun checkWithEqualLength(str1: String, str2: String): Boolean {
-    var diff = 0
-    for (i in str1.indices) {
-        if (str1[i] != str2[i]) diff++
-        if (diff > 1) return false
-    }
-    return true
-}
-
-private fun checkWithDifferentLength(str1: String, str2: String): Boolean {
-    val mStr1 = if (str1.length > str2.length) str1 else str2
-    val mStr2 = if (str1.length > str2.length) str2 else str1
-
-    var i = 0
-    var j = 0
-    while (i < mStr1.length && j < mStr2.length) {
-        if (mStr1[i] == mStr2[j]) j++
-        i++
-
-        if (i - j > 1) return false
-    }
-
-    return true
-}
-
-private fun oneEditAwayOnePass(str1: String, str2: String): Boolean {
-    if (abs(str1.length - str2.length) > 1) return false
-
-    val mStr1 = if (str1.length >= str2.length) str1 else str2
-    val mStr2 = if (str1.length >= str2.length) str2 else str1
-
-    var i = 0
-    var j = 0
-    var diff = 0
-    while (i < str1.length && j < str2.length) {
-        if (mStr1[i] != mStr2[j]) {
-            diff++
-            if (diff > 1) return false
-            if (mStr1.length == mStr2.length) j++
-        } else {
-            j++
+    var start = 0
+    var i = 1
+    while (i in str.indices) {
+        if (str[i] != str[start]) {
+            sb.append(i - start)
+            sb.append(str[i])
+            start = i
         }
         i++
     }
+    sb.append(i - start)
 
-    return true
+    return if (sb.length < str.length) {
+        sb.toString()
+    } else {
+        str
+    }
+}
+
+private fun compressString2(str: String): String {
+    val compressedLength = computeCompressedLength(str)
+    if (compressedLength >= str.length) return str
+
+    val sb = StringBuilder(compressedLength).also {
+        it.append(str[0])
+    }
+    var start = 0
+    var i = 1
+    while (i in str.indices) {
+        if (str[i] != str[start]) {
+            sb.append(i - start)
+            sb.append(str[i])
+            start = i
+        }
+        i++
+    }
+    sb.append(i - start)
+
+    return sb.toString()
+}
+
+private fun computeCompressedLength(str: String): Int {
+    var len = 0
+
+    var start = 0
+    var i = 1
+    while (i in str.indices) {
+        if (str[i] != str[start]) {
+            len += 1 + (i - start).length()
+            start = i
+        }
+        i++
+    }
+    len += 1 + (i - start).length()
+
+    return len
+}
+
+fun Int.length() = when (this) {
+    0 -> 1
+    else -> log10(abs(toDouble())).toInt() + 1
 }
