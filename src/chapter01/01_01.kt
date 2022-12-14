95291a80210b726b6a59e58a1f8c2a6bacc70f5f
iClaude <agostini.claudio1@gmail.com>
2022-12-14 14:10:42 +0100

    reformat exercise 1.1

--- src/chapter01/01_01.kt
+++ src/chapter01/01_01.kt
@@ -5,15 +5,6 @@ import java.util.*
 /* Is Unique: Implement an algorithm to determine if a string has all unique characters.
    What if you cannot use additional data structures? */
 
-fun main() {
-    val mStr = "abcdefghie"
-    println(hasUniqueCharactersWithArray(mStr))
-    println(hasUniqueCharactersWithMap(mStr))
-    println(hasUniqueCharactersWithSorting(mStr))
-    println(hasUniqueCharactersWithBits(mStr))
-    println(mStr.hasUniqueCharactersWithBitSet())
-}
-
 private fun hasUniqueCharactersWithArray(str: String): Boolean {
     if (str.length > 256) return false
 
@@ -83,4 +74,13 @@ fun String.hasUniqueCharactersWithBitSet(): Boolean {
         }
     }
     return true
+}
+
+fun main() {
+    val mStr = "abcdefghie"
+    println(hasUniqueCharactersWithArray(mStr))
+    println(hasUniqueCharactersWithMap(mStr))
+    println(hasUniqueCharactersWithSorting(mStr))
+    println(hasUniqueCharactersWithBits(mStr))
+    println(mStr.hasUniqueCharactersWithBitSet())
 }
