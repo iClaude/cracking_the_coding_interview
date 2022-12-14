abf6628f2bd1ada5db7e20dabc5298a7e2e116e5
iClaude <agostini.claudio1@gmail.com>
2022-12-14 13:44:46 +0100

    update exercise 1.1

--- src/chapter01/01_01.kt
+++ src/chapter01/01_01.kt
@@ -2,21 +2,72 @@ package chapter01
 
 import java.util.*
 
-/* PROBLEM
-   Is Unique: Implement an algorithm to determine if a string has all unique characters.
+/* Is Unique: Implement an algorithm to determine if a string has all unique characters.
    What if you cannot use additional data structures? */
 
-fun String.hasUniqueCharacters(): Boolean {
-    if (length > 256) return false
+fun main() {
+    val mStr = "abcdefghie"
+    println(hasUniqueCharactersWithArray(mStr))
+    println(hasUniqueCharactersWithMap(mStr))
+    println(hasUniqueCharactersWithSorting(mStr))
+    println(hasUniqueCharactersWithBits(mStr))
+    println(mStr.hasUniqueCharactersWithBitSet())
+}
 
-    val asciiChars = Array(256) { false }
-    for (char in this) {
-        if (asciiChars[char.code]) {
+private fun hasUniqueCharactersWithArray(str: String): Boolean {
+    if (str.length > 256) return false
+
+    val chars = Array(256) { false }
+
+    for (char in str) {
+        if (chars[char.code]) {
             return false
-        } else {
-            asciiChars[char.code] = true
+        }
+        chars[char.code] = true
+    }
+
+    return true
+}
+
+private fun hasUniqueCharactersWithMap(str: String): Boolean {
+    if (str.length > 256) return false
+
+    val chars = hashMapOf<Char, Boolean>()
+
+    for (char in str) {
+        if (chars[char] == true) {
+            return false
+        }
+        chars[char] = true
+    }
+
+    return true
+}
+
+private fun hasUniqueCharactersWithSorting(str: String): Boolean {
+    if (str.length > 256) return false
+
+    val strSorted = str.toCharArray().sorted()
+    for (i in 0..strSorted.size - 2) {
+        if (strSorted[i] == strSorted[i + 1]) {
+            return false
         }
     }
+
+    return true
+}
+
+private fun hasUniqueCharactersWithBits(str: String): Boolean {
+    var chars: Int = 0
+
+    for (char in str) {
+        val pos = char.code - 97
+        if ((1 shl (pos) and chars) > 0) {
+            return false
+        }
+        chars = 1 shl (pos) or chars
+    }
+
     return true
 }
 
@@ -32,28 +83,4 @@ fun String.hasUniqueCharactersWithBitSet(): Boolean {
         }
     }
     return true
-}
-
-// only for lowercase letters
-fun String.hasUniqueCharactersWithBitwiseOperations(): Boolean {
-    if (length > 256) return false
-
-    var letters = 0
-    for (letter in this) {
-        val shift = letter.code - 97
-        if ((letters and (1 shl shift)) > 0) {
-            return false
-        } else {
-            letters = letters or (1 shl shift)
-        }
-    }
-    return true
-}
-
-fun main() {
-    val myString = "abcdefhif"
-    println("Has string \"$myString\" all unique characters (hasUniqueCharacters)? ${myString.hasUniqueCharacters()}")
-    println("Has string \"$myString\" all unique characters (hasUniqueCharactersWithBitSet)? ${myString.hasUniqueCharactersWithBitSet()}")
-    println("Has string \"$myString\" all unique characters (hasUniqueCharactersWithBits)? ${myString.hasUniqueCharactersWithBitwiseOperations()}")
-
 }
